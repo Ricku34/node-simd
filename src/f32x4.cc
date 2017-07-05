dfe8c52bf7420cdb444785a21a6ed1aecdfb6a2e
eric <eric.hasdenteufel@geomatys.com>
2017-07-05 14:17:57 +0200

    set indexed interceptor for SIMD.Float32x4

--- src/f32x4.cc
+++ src/f32x4.cc
@@ -6,9 +6,6 @@ Float32x4::Float32x4(float x, float y, float z,float w) {
 	vec = _mm_set_ps( w, z, y , x );
 }
 
-Float32x4::Float32x4() {
-}
-
 
 Float32x4::~Float32x4() {
 }
@@ -20,6 +17,14 @@ void Float32x4::Init(v8::Local<v8::Object> exports) {
   v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
   tpl->SetClassName(Nan::New("Float32x4").ToLocalChecked());
   tpl->InstanceTemplate()->SetInternalFieldCount(1);
+  Nan::SetIndexedPropertyHandler(
+  	tpl->InstanceTemplate(),
+  	Float32x4::PropertyGetter,
+    Float32x4::PropertySetter,
+    Float32x4::PropertyQuery,
+    Float32x4::PropertyDeleter,
+    Float32x4::PropertyEnumerator
+  );
 
   v8::Local<v8::Function> cst = tpl->GetFunction();
   constructor.Reset(cst);
@@ -31,26 +36,22 @@ void Float32x4::Init(v8::Local<v8::Object> exports) {
 void Float32x4::New(const Nan::FunctionCallbackInfo<v8::Value>& info) {
   if (info.IsConstructCall()) {
     // Invoked as constructor: `new Float32x4(...)`
-
-    Float32x4* obj;
-    if(info.Length()==0) {
-     	obj = new Float32x4();
-    } else {
-    	double r0 = (info.Length() >= 1 && info[0]->IsNumber())? info[0]->NumberValue() : 0;
-    	double r1 = (info.Length() >= 2 && info[1]->IsNumber())? info[1]->NumberValue() : 0;
-    	double r2 = (info.Length() >= 3 && info[2]->IsNumber())? info[2]->NumberValue() : 0;
-    	double r3 = (info.Length() == 4 && info[3]->IsNumber())? info[3]->NumberValue() : 0;
-    	obj = new Float32x4(r0,r1,r2,r3);
-    }
+	double r0 = (info.Length() >= 1 && info[0]->IsNumber())? info[0]->NumberValue() : NAN;
+	double r1 = (info.Length() >= 2 && info[1]->IsNumber())? info[1]->NumberValue() : NAN;
+	double r2 = (info.Length() >= 3 && info[2]->IsNumber())? info[2]->NumberValue() : NAN;
+	double r3 = (info.Length() == 4 && info[3]->IsNumber())? info[3]->NumberValue() : NAN;
+	Float32x4* obj = new Float32x4(r0,r1,r2,r3);
 
     obj->Wrap(info.This());
     info.GetReturnValue().Set(info.This());
   } else {
     // Invoked as plain function `Float32x4(...)`, turn into construct call.
-    const int argc = 1;
-    v8::Local<v8::Value> argv[argc] = { info[0] };
+    v8::Local<v8::Value> argv[info.Length()];
+    for(int i=0;i<info.Length();i++) {
+    	argv[i]=info[i];
+    }
     v8::Local<v8::Function> cons = Nan::New<v8::Function>(constructor);
-    info.GetReturnValue().Set(cons->NewInstance(argc, argv));
+    info.GetReturnValue().Set(cons->NewInstance(info.Length(), argv));
   }
 }
 
@@ -85,4 +86,38 @@ void Float32x4::Abs(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 	Float32x4* r = ObjectWrap::Unwrap<Float32x4>(res);
 	r->vec = _mm_max_ps(_mm_sub_ps(_mm_setzero_ps(), a->vec), a->vec);
 	info.GetReturnValue().Set(res);
+}
+
+NAN_INDEX_GETTER(Float32x4::PropertyGetter) {
+	float data[4];
+	Float32x4* r = ObjectWrap::Unwrap<Float32x4>(info.This());
+	_mm_store_ps(data, r->vec );
+    info.GetReturnValue().Set(Nan::New(data[index]));
+}
+
+NAN_INDEX_SETTER(Float32x4::PropertySetter) {
+	float data[4];
+	Float32x4* r = ObjectWrap::Unwrap<Float32x4>(info.This());
+	_mm_store_ps(data, r->vec );
+	data[index] = value->NumberValue();
+	r->vec = _mm_load_ps(data);
+	info.GetReturnValue().Set(info.This());
+}
+
+NAN_INDEX_ENUMERATOR(Float32x4::PropertyEnumerator) {
+
+	v8::Local<v8::Array> arr = Nan::New<v8::Array>();
+	Nan::Set(arr, 0, Nan::New(0));
+	Nan::Set(arr, 1, Nan::New(1));
+	Nan::Set(arr, 2, Nan::New(2));
+	Nan::Set(arr, 3, Nan::New(3));
+	info.GetReturnValue().Set(arr);
+}
+
+NAN_INDEX_DELETER(Float32x4::PropertyDeleter) {
+  info.GetReturnValue().Set(Nan::True());
+}
+
+NAN_INDEX_QUERY(Float32x4::PropertyQuery) {
+
 }
--- src/f32x4.h
+++ src/f32x4.h
@@ -5,22 +5,27 @@
 #include <mmintrin.h>
 
 class Float32x4 : public Nan::ObjectWrap {
- public:
-  static void Init(v8::Local<v8::Object> exports);
+	public:
+		static void Init(v8::Local<v8::Object> exports);
 
- private:
-  explicit Float32x4();
-  explicit Float32x4(float x, float y = 0.f, float z = 0.f,float w = 0.f);
+	private:
+		explicit Float32x4(float x= NAN, float y = NAN, float z = NAN,float w = NAN);
 
-  ~Float32x4();
+		~Float32x4();
 
-  static void New(const Nan::FunctionCallbackInfo<v8::Value>& info);
+		static void New(const Nan::FunctionCallbackInfo<v8::Value>& info);
 
-  static void Add(const Nan::FunctionCallbackInfo<v8::Value>& info);
-  static void Abs(const Nan::FunctionCallbackInfo<v8::Value>& info);
+		static NAN_INDEX_GETTER(PropertyGetter);
+		static NAN_INDEX_SETTER(PropertySetter);
+		static NAN_INDEX_ENUMERATOR(PropertyEnumerator);
+		static NAN_INDEX_DELETER(PropertyDeleter);
+		static NAN_INDEX_QUERY(PropertyQuery);
 
-  static Nan::Persistent<v8::Function> constructor;
-  __m128 vec;
+		static void Add(const Nan::FunctionCallbackInfo<v8::Value>& info);
+		static void Abs(const Nan::FunctionCallbackInfo<v8::Value>& info);
+
+		static Nan::Persistent<v8::Function> constructor;
+		__m128 vec;
 };
 
 #endif
